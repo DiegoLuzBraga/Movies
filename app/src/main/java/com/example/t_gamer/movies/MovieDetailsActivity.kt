9a68adbd91fb792746675e77057b37b940150c05
Diego <diego.luz.braga@gmail.com>
2018-12-20 23:32:51 -0300

    - Change: style changes

--- app/src/main/java/com/example/t_gamer/movies/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/t_gamer/movies/MovieDetailsActivity.kt
@@ -3,20 +3,26 @@ package com.example.t_gamer.movies
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_movie_details.*
+import kotlinx.android.synthetic.main.custom_toolbar.*
 
 class MovieDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_movie_details)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+        val overview: String = intent.getStringExtra("overview")
+        val image: String = intent.getStringExtra("image")
 
-            val overview: String = intent.getStringExtra("overview")
-            val image: String = intent.getStringExtra("image")
-            supportActionBar?.title = intent.getStringExtra("title")
+        supportActionBar?.title = intent.getStringExtra("title")
 
+        if (overview.isNotEmpty()) {
             overviewTXT.text = overview
-            Picasso.get().load(image).into(detailsIMG)
+        } else {
+            overviewTXT.text = getString(R.string.descriptionUnavailable)
+        }
+        Picasso.get().load(image).into(detailsIMG)
     }
 }
